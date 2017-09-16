f1ad8fae6c708fb38680e37b56b00547d8edf6ee
sruthib1 <32008263+sruthib1@users.noreply.github.com>
2017-09-16 13:37:07 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+    StudentGroup a=new StudentGroup(100);
+        Student b;
+        int x=0;
+       b=a.getStudent(x);
+    System.out.println("main");
+
+
+	}
+
+          }
